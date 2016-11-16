0b206c83de231a1a3b2b9c25caa0630fc63b38f3
Fortune <fortune.soleil@gmail.com>
2016-11-15 19:43:38 -0500

    updated mad late

---
+++ sig.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+
+static void sighandler( int sig ){
+  if (sig == SIGINT){
+    int f = open("sigfile.txt", O_CREAT|O_WRONLY|O_APPEND, 0644);
+    char txt[] = "exited process\n";
+    write(f, txt, sizeof(txt));
+    close(f);
+    exit(1);
+  }
+  else if (sig == SIGUSR1){
+    printf("parental P-I-D appears to be: %d\n", getppid());
+  }
+}
+
+int main(){
+  signal(SIGINT, sighandler);
+  signal(SIGUSR1, sighandler);
+  while(1){
+    printf("the P-I-D of me appears to be: %d\n", getpid());
+    sleep(1);
+  }
+}
